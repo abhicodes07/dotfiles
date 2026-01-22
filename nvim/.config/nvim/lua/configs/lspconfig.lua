1beeddce66ed8617a768404b2cf7224d2bb0adbf
Abhinav <abhiasthana76@gmail.com>
2026-01-22 08:20:08 +0530

    feat(lsp): :sparkles: add jinja template lsp

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "htmldjango", "jinja", "jinja2", "pyright" }
+local servers = { "html", "cssls", "ruff", "htmldjango", "jinja", "jinja2", "pyright", "tailwindcss-lsp-server"}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
